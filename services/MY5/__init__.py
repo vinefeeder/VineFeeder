103f7da1a16a0436fa17ddfa6f817061cb98a3b0
A_n_g_e_l_a <angelaslaney@gmail.com>
2024-11-05 15:47:02 +0000

    Add files via upload

--- services/MY5/__init__.py
+++ services/MY5/__init__.py
@@ -1,5 +1,13 @@
 from base_loader import BaseLoader
 from parsing_utils import rinse
+import jmespath
+from rich.console import Console
+import sys  
+import re
+from parsing_utils import parse_json, extract_params_json
+import subprocess
+
+console = Console()
 
 class My5Loader(BaseLoader):
     def __init__(self):
@@ -11,12 +19,222 @@ class My5Loader(BaseLoader):
         }
         super().__init__(headers)
         
-    def receive(self, inx: None, search_term: None):
-    	print("Not implemented. This is a skeleton only.")
+    def receive(self, inx: None, search_term: None, category=None):
+       
+        """
+        First fetch for series titles matching all or part of search_term.
+        
+        Uses inx, an int variable to switch:-
+            0 for greedy search using url
+            1 for direct url download
+            2 for category browse 
+            3 for search with keyword
+        If search_url_entry is left blank vinefeeder generates a 
+        a menu with 3 options:
+            - Greedy Search by URL
+            - Browse by Category
+            - Search by Keyword
+        results are forwarded to receive().
+        If inx == 1, do direct download using url.
+        If inx == 3, do keyword search.
+        If inx == 0, fetch videos using a greedy search or browse-category.
+        If inx == 2, fetch videos from a category url.
+        If an unknown error occurs, exit with code 0.
+        """
+        # direct download
+
+        if 'http' in search_term and inx == 1:
+            #print(['devine', 'dl', 'MY5', search_term])
+            subprocess.run(['devine', 'dl', 'MY5', search_term])  # url
+            
+            return
+
+        # keyword search
+        elif inx == 3:
+            print(f"Searching for {search_term}")
+            return (self.fetch_videos(search_term))  
+        
+        # ALTERNATIVES BELOW FROM POP-UP MENU  
+        elif inx == 0:  
+            # from greedy-search OR selecting Browse-category
+            # example: https://www.channel4.com/programmes/the-great-british-bake-off/on-demand/75228-001
+
+            # need a search keyword(s) from url 
+            # split and select series name
+            search_term = search_term.split('/')[3].replace('-',' ') 
+            # fetch_videos_by_category search_term may have other params to remove
+            if '/' in search_term:  
+                search_term = search_term.split('/')[0].replace('-',' ')
+            return (self.fetch_videos(search_term))
+        
+        elif 'http' in search_term and inx == 2:
+            self.category = category
+            self.fetch_videos_by_category(search_term)  # search_term here holds a URL!!!
+            
+        else:
+            print(f"Unknown error when searching for {search_term}")
+            
+        # prepare terminal for next run    
+    
+        print(f"[info] Finished downloading for {search_term}")
+        print("[info] Ready: waiting for service selection...")
+        #return self.clean_terminal()
+        return
+         
     	
     def fetch_videos(self, search_term):
         """Fetch videos from ITV using a search term."""
-        print("Not implemented. This is a skeleton only.")
-    	
+        url = f"https://corona.channel5.com/shows/search.json?platform=my5desktop&friendly=1&query={search_term}"
+        response = self.get_data(url, headers=self.headers)
+        parsed_data = parse_json(response)
+        #console.print_json(data=parsed_data)
+        
+        if parsed_data and 'shows' in parsed_data:
+            for item in parsed_data['shows']:
+                series_name = item.get('title', 'Unknown Series')
+                episode = {
+                    'title': item.get('title', 'Unknown Title'),
+                    'slug': f"{item.get('f_name', '')}",
+                    'synopsis': f"{item.get('s_desc', '')}",
+                    'url': f"https://corona.channel5.com/shows/{item.get('f_name', '')}/seasons.json?platform=my5desktop&friendly=1"
+                }
+                self.add_episode(series_name, episode)
+
+        # List series and episodes using beaupy
+        selected_series = self.display_series_list()
+        if selected_series:
+            return self.second_fetch(selected_series)  # Return URLs for selected episodes
+        return None
+        
+    def second_fetch(self, selected):
+        """
+        Given a selected series name, fetch its HTML and extract its episodes.
+        Or if given a direct url, fetch it and process for greedy download.
+        The function will prepare the series data for episode selection and display the final episode list.
+        It will return the URLs for the selected episodes.
+        """
+        if 'https' in selected:  # direct url: not sure how this may happen with My5??
+            url = selected
+            subprocess.run(['devine', 'dl', 'MY5', url])
+            return
+        else:
+            url = self.get_selected_url(selected)
+        try:
+            myhtml = self.get_data(url=url)
+        except:
+            print(f"No valid data at {url} found.\n Exiting")
+            sys.exit(0)
+
+        parsed_data = parse_json(myhtml)
+        console.print_json(data=parsed_data)
+        self.clear_series_data()  # Clear existing series data
+
+        # Extract the episodes from the parsed data of the selected series
+        brndslug = selected.lower().replace(' ','-').replace(':','-').replace("'",'-').replace('--','-')    
+        #brndslug = re.sub(r"[^\w\s]", "-", selected.lower()).replace(" ", "-").replace('--','-')
+
+        if parsed_data and 'seasons' in parsed_data:
+            for item in parsed_data['seasons']: 
+                try:
+                    url = f"https://corona.channel5.com/shows/{brndslug}/seasons/{item['seasonNumber']}/episodes.json?platform=my5desktop&friendly=1&linear=true"
+                    url = url.encode('utf-8', 'ignore').decode().strip() 
+                    myhtml = self.get_data(url=url)
+                    parsed_data = parse_json(myhtml)
+                    if len(parsed_data['episodes']) == 0:
+                        brndslug = brndslug + "-cutdowns"
+                        url = f"https://corona.channel5.com/shows/{brndslug}/seasons/{item['seasonNumber']}/episodes.json?platform=my5desktop&friendly=1&linear=true"
+                        url = url.encode('utf-8', 'ignore').decode().strip() 
+                        myhtml = self.get_data(url=url)
+                        parsed_data = parse_json(myhtml)
+
+                    console.print_json(data=parsed_data)
+                    series_name = parsed_data['episodes'][0]['sh_f_name'] 
+                    for item in parsed_data['episodes']:
+                        
+                        episode = {
+                            'series_no': f"{item['sea_num'] or '00'}",
+                            'title': f"{item['ep_num'] or ''}:{item['title'] or ''}",
+                            'url': f"https://www.channel5.com/{item['sh_f_name']}/{item['sea_f_name']}/{item['f_name']}",
+                            'synopsis': item['s_desc'] or None,
+                        }
+                        self.add_episode(series_name, episode)
+                except KeyError as e:
+                    print(f"Error: {e}")
+                    continue  # Skip any episode that doesn't have the required information   
+        
+        self.prepare_series_for_episode_selection(series_name) # creates list of series; allows user selection of wanted series prepares an episode list over chosen series
+        selected_final_episodes = self.display_final_episode_list(self.final_episode_data)
+
+        # specific to MY5
+        for item in selected_final_episodes:
+            url = item.split(',')[2].lstrip()
+            if url  == 'None':
+                print(f"No valid URL for {item.split(',')[1]}")
+                continue
+    
+            # debug uncomment
+            #print(url)
+
+            # finally fetch video
+            subprocess.run(['devine', 'dl', 'MY5', url])
+            #self.clean_terminal()
+            return
+        
+    def fetch_videos_by_category(self, browse_url):
+        """
+        Fetches videos from a category (Channel 4 specific).
+        Args:
+            browse_url (str): URL of the category page.
+        Returns:
+            None
+        """
+        beaupylist = []
+        try:
+            req = self.get_data(browse_url, headers=self.headers)
+            
+            # Parse the json data 
+            init_data = parse_json(req)
+            #console.print_json(data=init_data)
+            # Extract brand items
+            myjson = init_data['shows']
+
+            # jmespath is an efficient json parser that searches complex json
+            # and, in this case, produces a simple dict from which
+            # res(ults) are more easily extracted.
+            # C4 specific
+            res = jmespath.search("""
+            [*].{
+                href: f_name,
+                label: title,
+                overlaytext: s_desc            
+            } """, myjson)
+
+            # Build the beaupylist for display
+            for i, item in enumerate(res):
+                label = item['label']
+                overlaytext = item['overlaytext']
+                beaupylist.append(f"{i} {label}\n\t{overlaytext}") # \t used to split text later
+
+        except Exception as e:
+            print(f"Error fetching category data: {e}")
+            sys.exit(0)
+        
+        # call function in BaseLoader 
+        found = self.display_beaupylist(beaupylist)
+        
+        if found:
+            ind = found.split(' ')[0]
+            url = f"https://www.channel5.com/show/{res[int(ind)]['href']}"
+            # url may be for series or single Film
+            url = url.encode('utf-8', 'ignore').decode().strip()  # has spaces!
+
+            # process short-cut download or do greedy search on url
+            return self.process_received_url_from_category(url)
+            
+        else:
+            print("No video selected.")
+            sys.exit(0)
+
+            
     	
 
