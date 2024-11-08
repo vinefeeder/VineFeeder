06cdf1e26509939813c1037c351c2f638179e370
A_n_g_e_l_a <angelaslaney@gmail.com>
2024-11-08 13:43:13 +0000

    Add files via upload

--- services/U/__init__.py
+++ services/U/__init__.py
@@ -1,5 +1,12 @@
 from base_loader import BaseLoader
 from parsing_utils import rinse
+import jmespath
+import sys
+import json
+import subprocess
+from rich.console import Console
+
+console = Console()
 
 class ULoader(BaseLoader):
     def __init__(self):
@@ -11,12 +18,173 @@ class ULoader(BaseLoader):
         }
         super().__init__(headers)
         
-    def receive(self, inx: None, search_term: None):
-    	print("Not implemented. This is a skeleton only.")
+    def receive(self, inx: None, search_term: None, category=None):
+        """
+        First fetch for series titles matching all or part of search_term.
+        
+        Uses inx, an int variable to switch:-
+            0 for greedy search using url
+            1 for direct url download
+            2 for category browse 
+            3 for search with keyword
+        If search_url_entry is left blank vinefeeder generates a 
+        a menu with 3 options:
+            - Greedy Search by URL
+            - Browse by Category
+            - Search by Keyword
+        results are forwarded to receive().
+        If inx == 1, do direct download using url.
+        If inx == 3, do keyword search.
+        If inx == 0, fetch videos using a greedy search or browse-category.
+        If inx == 2, fetch videos from a category url.
+        If an unknown error occurs, exit with code 0.
+        """
+        # direct download
+        if 'http' in search_term and inx == 1:
+            
+            subprocess.run(['devine', 'dl', 'UKTV', search_term])  # url
+            
+            return
+
+        # keyword search
+        elif inx == 3:
+            print(f"Searching for {search_term}")
+            return (self.fetch_videos(search_term))  
+        
+        # ALTERNATIVES BELOW FROM POP-UP MENU  
+        elif inx == 0:  
+            # from greedy-search OR selecting Browse-category
+            # example  https://u.co.uk/shows/worlds-most-dangerous-roads/series-6/episode-1/6363704256112
+
+            # need a search keyword(s) from url 
+            # split and select series name
+            search_term = search_term.split('/')[4].replace('-',' ') 
+            return (self.fetch_videos(search_term))
+
+        
+        elif 'http' in search_term and inx == 2:
+            self.category = category
+            self.fetch_videos_by_category(search_term)  # search_term here holds a URL!!!
+            
+        else:
+            print(f"Unknown error when searching for {search_term}")
+            
+        # prepare terminal for next run    
+    
+        print(f"[info] Finished downloading for {search_term}")
+        print("[info] Ready: waiting for service selection...")
+        #return self.clean_terminal()
+        return
     	
     def fetch_videos(self, search_term):
         """Fetch videos from ITV using a search term."""
-        print("Not implemented. This is a skeleton only.")
-    	
-    	
+        headers={
+            'user-agent': 'Dalvik/2.9.8 (Linux; U; Android 9.9.2; ALE-L94 Build/NJHGGF)',
+            'Host': 'vschedules.uktv.co.uk',
+            'Origin':'https://uktv.co.uk',
+            'Referer':'https://uktv.co.uk/',
+        }
+        url = f"https://vschedules.uktv.co.uk/vod/search/?q={search_term}"
+        
+        html = self.get_data(url, headers)
+        parsed_data = self.parse_data(html)  # to json ()
+        
+    
+        # select only from FREE tier
+        res = jmespath.search("""
+            [].{
+            title: name,
+            slug: slug,
+            synopsis: synopsis
+            } """,  parsed_data)
 
+        for item in res:
+            title = item['title']
+            slug = item['slug']
+            synopsis = item['synopsis']
+            url = f"https://vschedules.uktv.co.uk/vod/brand/?slug={slug}"
+
+            episode = {
+                    'title': item.get('title', 'Unknown Title'),
+                    'url': url,
+                    'synopsis': synopsis
+                }
+            self.add_episode(title, episode)
+
+    	 # List series with search_term using beaupy
+        selected_series = self.display_series_list()
+        
+        if selected_series:
+            return self.second_fetch(selected_series)
+        return None
+    
+    def second_fetch(self, selected):
+        """
+        Given a selected series name, fetch its HTML and extract its episodes.
+        Or if given a direct url, fetch it and process for greedy download.
+        The function will prepare the series data for episode selection and display the final episode list.
+        It will return the URLs for the selected episodes.
+        """
+        if 'https' in selected:  # direct url provided skip url preparation
+            url = selected
+        else:
+            url = self.get_selected_url(selected)
+        try: 
+            myhtml = self.get_data(url)
+        except:
+            print(f"No valid data at {url} found.\n Exiting")
+            sys.exit(0)
+        parsed_data = self.parse_data(myhtml)
+        '''console.print_json(data=parsed_data)
+        f = open('u.json', 'w')
+        f.write(json.dumps(parsed_data))
+        f.close()'''
+        self.clear_series_data()  # Clear existing series data
+
+        
+        
+        series_ids=[series['id'] for series in parsed_data['series']]
+
+        # Go through each episode in the selected series
+        for series_id in series_ids:
+            html = self.get_data(f"https://vschedules.uktv.co.uk/vod/series/?id={series_id}")
+            parsed_data = self.parse_data(html)
+
+            '''console.print_json(data=parsed_data)
+            f = open('2u.json', 'w') 
+            f.write(json.dumps(parsed_data))
+            f.close()'''
+
+            try:
+                for item in parsed_data['episodes']:
+                    #series_id = item['series_id']
+                    ep_number = item['episode_number']
+                    ser_number = item['series_number']
+                    video_id = item['video_id']
+                    brand_slug = item['brand_slug']
+                    url = f"https://u.co.uk/shows/{brand_slug}/series-{ser_number}/episode-{ep_number}/{video_id}"
+                    synopsis = item['synopsis']
+                    
+                    episode = {
+                        'series_no': ser_number,
+                        'title': ep_number,
+                        'url': url,
+                        'synopsis': synopsis
+                    }
+                    self.add_episode(brand_slug, episode)
+     
+            except KeyError:
+                continue  # Skip any episode that doesn't have the required information
+
+        self.prepare_series_for_episode_selection(brand_slug) # creates list of series; allows user selection of wanted series prepares an episode list over chosen series
+        selected_final_episodes = self.display_final_episode_list(self.final_episode_data)
+        for item in selected_final_episodes:
+            url = item.split(',')[2].lstrip()
+            subprocess.run(['devine', 'dl', 'UKTV', url])
+            
+        return None
+    
+
+    def fetch_videos_by_category(self, browse_url):
+        print("Method not implemented for fetching videos by category for this channel.")
+        return None
